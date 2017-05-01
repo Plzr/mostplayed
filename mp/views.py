267f25a512a7187be9ef934a34ce6cee8ace897a
Si Quick <siquick@gmail.com>
2017-05-02 07:45:53 +1000

    removed subscription scope

--- mp/views.py
+++ mp/views.py
@@ -38,7 +38,7 @@ def go():
 	else:
 		session['num_tracks'] = '25'
 	callback_url = request.url_root + 'callback'
-	base_url = 'https://accounts.spotify.com/en/authorize?client_id=' + client_id + '&response_type=code&redirect_uri=' + callback_url + '&scope=user-read-private%20user-read-email%20playlist-read-private%20user-follow-read%20user-library-read%20user-top-read%20playlist-modify-private%20playlist-modify-public&state=34fFs29kd09'
+	base_url = 'https://accounts.spotify.com/en/authorize?client_id=' + client_id + '&response_type=code&redirect_uri=' + callback_url + '&scope=user-read-email%20playlist-read-private%20user-follow-read%20user-library-read%20user-top-read%20playlist-modify-private%20playlist-modify-public&state=34fFs29kd09'
 	return redirect(base_url,302)
 
 
